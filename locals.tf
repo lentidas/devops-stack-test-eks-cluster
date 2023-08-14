9a621e23b7003ba10447acf506ba9dc22d6dadcd
lentidas <33546359+lentidas@users.noreply.github.com>
2023-08-14 15:04:46 +0200

    docs: add comment

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   cluster_name           = "gh-eks-cluster"
   base_domain            = "is-sandbox.camptocamp.com"
   cluster_issuer         = "letsencrypt-staging"
-  enable_service_monitor = false
+  enable_service_monitor = false # Can be enabled after the first bootstrap
   app_autosync           = true ? { allow_empty = false, prune = true, self_heal = true } : {}
 
   vpc_cidr            = "10.56.0.0/16"
