0ea232f58a900ddfd12575dbcf46235dcb8af2cd
lentidas <33546359+lentidas@users.noreply.github.com>
2023-12-01 10:06:43 +0100

    feat: add local with Let's Encrypt e-mail

--- locals.tf
+++ locals.tf
@@ -1,10 +1,11 @@
 locals {
-  kubernetes_version     = "1.28"
-  cluster_name           = "gh-eks-cluster"            # Must be unique for each DevOps Stack deployment in a single AWS account.
-  base_domain            = "is-sandbox.camptocamp.com" # Must match a Route 53 zone in the AWS account where you are deploying the DevOps Stack.
-  cluster_issuer         = module.cert-manager.cluster_issuers.staging
-  enable_service_monitor = false # Can be enabled after the first bootstrap.
-  app_autosync           = true ? { allow_empty = false, prune = true, self_heal = true } : {}
+  kubernetes_version       = "1.28"
+  cluster_name             = "gh-eks-cluster"            # Must be unique for each DevOps Stack deployment in a single AWS account.
+  base_domain              = "is-sandbox.camptocamp.com" # Must match a Route 53 zone in the AWS account where you are deploying the DevOps Stack.
+  cluster_issuer           = module.cert-manager.cluster_issuers.staging
+  letsencrypt_issuer_email = "letsencrypt@camptocamp.com"
+  enable_service_monitor   = false # Can be enabled after the first bootstrap.
+  app_autosync             = true ? { allow_empty = false, prune = true, self_heal = true } : {}
 
   # The VPC CIDR must be unique for each DevOps Stack deployment in a single AWS account.
   vpc_cidr = "10.56.0.0/16"
